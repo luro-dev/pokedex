95a08d66e46081b7c6bd629940733cd9638048b4
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-27 12:03:36 -0400

    Set up project files

---
+++ src/ components/PokeCard.jsx
@@ -0,0 +1,3 @@
+export function PokeCard() {
+  return <div></div>;
+}
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./ components/Header";
+import { Modal } from "./ components/Modal";
+import { PokeCard } from "./ components/PokeCard";
+import { SideNav } from "./ components/SideNav";
+import { TypeCard } from "./ components/TypeCard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SideNav />
+      <PokeCard />
+    </>
+  );
+}
+
+export default App;
